14bcf89782500801d69459168cf750405fa403a7
Kangning Li <likangning93@gmail.com>
2016-09-28 23:06:59 -0400

    init with basecode

---
+++ CIS568-PaintBall-Framework/Assets/Scripts/MobileNetwork.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MobileNetwork : Photon.PunBehaviour
+{
+ 
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    // TODO-2.a: the same as 1.b
+    //   and join a room
+
+
+    //public override void OnJoinedRoom()
+    //{
+    //    GetComponent<MobileShooter>().Activate();
+    //}
+
+
+}
