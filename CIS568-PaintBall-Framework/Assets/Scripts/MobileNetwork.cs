6869617c7388b49499057f358701691882582222
Kangning Li <likangning93@gmail.com>
2016-09-29 01:22:43 -0400

    part 2 is networking and targeting correctly

--- CIS568-PaintBall-Framework/Assets/Scripts/MobileNetwork.cs
+++ CIS568-PaintBall-Framework/Assets/Scripts/MobileNetwork.cs
@@ -6,16 +6,26 @@ public class MobileNetwork : Photon.PunBehaviour
     void OnGUI()
     {
         GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        GUILayout.Label("Room Name: " + roomName);
+    }
+
+    string roomName;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
     }
 
     // TODO-2.a: the same as 1.b
     //   and join a room
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
 
-
-    //public override void OnJoinedRoom()
-    //{
-    //    GetComponent<MobileShooter>().Activate();
-    //}
-
-
+    public override void OnJoinedRoom()
+    {
+        roomName = PhotonNetwork.room.name;
+        //GetComponent<MobileShooter>().Activate();
+    }
 }
